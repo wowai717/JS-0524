666d366005f8c16f755a2d2bb0b59ac374f41bcc
WOOJUN NAM <bjacau3@gmail.com>
2025-05-27 00:22:33 +0900

    node.js
    맛보기

--- section03/src/index.js
+++ section03/src/index.js
@@ -1,1 +1,9 @@
-console.log("안녕 Node.js");
+// const{add, sub} = require("./math")
+
+import {add, sub} from "./math.js";
+import mul from "./math.js";
+
+console.log (add(1,2));
+console.log (sub(1,2));
+console.log (mul(2,3));
+// console.log(moduleData);
